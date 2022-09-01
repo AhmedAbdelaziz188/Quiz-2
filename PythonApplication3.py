90db813d4e68481876e00c6839f6f67d55644155
AhmedAbdelaziz188 <111662569+AhmedAbdelaziz188@users.noreply.github.com>
2022-09-01 22:35:25 +0200

    Add files via upload

---
+++ PythonApplication3.py
@@ -0,0 +1,27 @@
+num_list = [0, 0, 0]
+num1=[[0,0,0]]
+num2=num_list
+q2_li5t=[5,3,4,6,1]
+q2_li5t.sort()
+print(q2_li5t)
+word = "OLLEH"
+for slicing in word:
+    print(slicing)
+text = ["h","e","l","l","o"]
+print(text[4])
+lst = [1,2,3,4,5]
+print(lst.append([6,7,8]))
+tople=("apple","Cherry","banana")
+print(tople)
+lsttople=[("apple","Cherry","banana")]
+print(lsttople)
+d ={"v":"S001",
+    "V":"s002",
+    "VI":"s001",
+    "vi":"s005",
+    "VIII":"s007"
+}
+list=[1,2,2,33,4,4,11,22,3,3,2]
+unique_list=[] for a in list:
+     if a not in unique_list: unique_list.append(a)
+print(unique_list)
